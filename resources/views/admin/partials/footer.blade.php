25aff91344b736ce1872a1700a2a150a98c6f038
Dilan Zeledón <dilanzq@outlook.com>
2025-06-05 21:25:23 -0400

    Primer commit desde servidor

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="copyright-wrapper">
+    <div class="copyright-area">
+        <p><a href="">3lite.</a></p>
+        <p> <span>{{ $basic_settings->site_name }} v{{ $basic_settings->web_version }}</span></p> 
+    </div>
+</div>
